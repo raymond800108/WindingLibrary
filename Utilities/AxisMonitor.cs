3e31343049fce1848c24404c7468d1fc2622e65b
t3ch-support <pneucolas@gmail.com>
2019-08-15 13:55:05 +0200

    read me updated

--- Utilities/AxisMonitor.cs
+++ Utilities/AxisMonitor.cs
@@ -53,83 +53,157 @@ public partial class AxisAngleChecker : GH_ScriptInstance
     #endregion
 
 
-    private void RunScript(double Axis1, double Axis2, double Axis3, double Axis4, double Axis5, double Axis6, ref object Axis1Bool, ref object Axis2Bool, ref object Axis3Bool, ref object Axis4Bool, ref object Axis5Bool, ref object Axis6Bool, ref object masterColor)
+    private void RunScript(double A1, double A2, double A3, double A4, double A5, double A6, double E1_Rail, double E2_Positioner, bool iReset, ref object axis1Bool, ref object axis2Bool, ref object axis3Bool, ref object axis4Bool, ref object axis5Bool, ref object axis6Bool, ref object railAxisBool, ref object positionerAxisBool, ref object oErrorAxisColorIndex, ref object oSingularityAlert, ref object oSingularityCount, ref object oAxisProbCount)
 
     {
         // <Custom code>
-        masterColor = 1;
-
-        // Axis1: Range +/-185
-        if (Axis1 >= -185 && Axis1 <= 185)
+        double axis1Angle = A1;
+        double axis2Angle = A2;
+        double axis3Angle = A3;
+        double axis4Angle = A4;
+        double axis5Angle = A5;
+        double axis6Angle = A6;
+        double railAxisValue = E1_Rail;
+        double positionerAxisVlaue = E2_Positioner;
+        if (iReset)
         {
-            Axis1Bool = 1;
+            singularityCount = 0;
+            axisProbCount = 0;
+        }
+        // KUKA KR420 R3080
+        // Color index: Axis1 ~ Axis6 = 0~5 , railAxis: 6 , positionerAxis: 7
+
+        List<int> axisColorIndex = new List<int>();
+
+        // Singularity Color index: Axis1 ~ Axis6 = 0~5 , railAxis: 6 , positionerAxis: 7
+
+        List<string> SingularityAxisTest = new List<string>();
+
+
+        //Axis 1
+        if (-185 <= A1 && A1 <= 185)
+        {
+            axis1Bool = true;
         }
         else
         {
-            Axis1Bool = 0;
-            masterColor = 0;
+            axis1Bool = false;
+            axisColorIndex.Add(0);
+            axisProbCount++;
         }
 
-        // Axis2: Range -140/-5
-        if (Axis1 >= -140 && Axis1 <= -5)
+        //Axis 2
+        if (-130 <= axis2Angle && axis2Angle <= 20)
         {
-            Axis1Bool = 1;
+            axis2Bool = true;
         }
         else
         {
-            Axis1Bool = 0;
-            masterColor = 0;
+            axis2Bool = false;
+            axisColorIndex.Add(1);
+            axisProbCount++;
         }
 
-        // Axis3: Range -120/+155
-        if (Axis1 >= -120 && Axis1 <= 155)
+        //Axis 3
+        if (-100 <= axis3Angle && axis3Angle <= 144)
         {
-            Axis1Bool = 1;
+            axis3Bool = true;
+
+            if (-1 <= axis3Angle && axis3Angle <= 1)
+            {
+                SingularityAxisTest.Add("A6, A3, A2 almost coplanar: Elbow Singularity Alert");
+                singularityCount++;
+            }
         }
         else
         {
-            Axis1Bool = 0;
-            masterColor = 0;
+            axis3Bool = false;
+            axisColorIndex.Add(2);
+            axisProbCount++;
         }
 
-        // Axis4: Range +/-350
-        if (Axis1 >= -350 && Axis1 <= 350)
+        //Axis 4
+        if (-350 <= axis4Angle && axis4Angle <= 350)
         {
-            Axis1Bool = 1;
+            axis4Bool = true;
         }
         else
         {
-            Axis1Bool = 0;
-            masterColor = 0;
+            axis4Bool = false;
+            axisColorIndex.Add(3);
+            axisProbCount++;
         }
 
-        // Axis5: Range +/-122.5
-        if (Axis1 >= -115 && Axis1 <= 115)
+        //Axis 5
+        if (-120 <= axis5Angle && axis5Angle <= 120)
         {
-            Axis1Bool = 1;
+            axis5Bool = true;
+            if (-1 <= axis5Angle && axis5Angle <= 1)
+            {
+                SingularityAxisTest.Add("A6 align with A4: Wrist Singularity Alert");
+                singularityCount++;
+            }
         }
         else
         {
-            Axis1Bool = 0;
-            masterColor = 0;
+            axis5Bool = false;
+            axisColorIndex.Add(4);
+            axisProbCount++;
         }
 
-        // Axis6: Range +/-350
-        if (Axis1 >= -350 && Axis1 <= 350)
+        //Axis 6
+        if (-350 <= axis6Angle && axis6Angle <= 350)
         {
-            Axis1Bool = 1;
+            axis6Bool = true;
         }
         else
         {
-            Axis1Bool = 0;
-            masterColor = 0;
+            axis6Bool = false;
+            axisColorIndex.Add(5);
+            axisProbCount++;
         }
 
-        Print(masterColor.ToString());
+        //Rail Axis, Unit:meter(0.0~11.0), not quite sure the rail type and distance limitation
+        if (0 <= railAxisValue && railAxisValue <= 11.000)
+        {
+            railAxisBool = true;
+        }
+        else
+        {
+            railAxisBool = false;
+            axisColorIndex.Add(6);
+            axisProbCount++;
+        }
+
+        //Positioner Axis
+        if (-350 <= positionerAxisVlaue && positionerAxisVlaue <= +350)
+        {
+            positionerAxisBool = true;
+        }
+        else
+        {
+            positionerAxisBool = false;
+            axisColorIndex.Add(7);
+            axisProbCount++;
+        }
+
+        // Shoulder Singularity
+        double ShoulderSingularity = axis2Angle + axis3Angle;
+        if (-90 <= ShoulderSingularity && ShoulderSingularity <= -70)
+        {
+            SingularityAxisTest.Add("A1 almost align with A6: Shoulder Singularity Alert");
+            singularityCount++;
+        }
+
+        oErrorAxisColorIndex = axisColorIndex;
+        oSingularityAlert = SingularityAxisTest;
+        oSingularityCount = singularityCount;
+        oAxisProbCount = axisProbCount;
         // </Custom code>
     }
 
     // <Custom additional code>
-    
+    int singularityCount = 0;
+    int axisProbCount = 0;
     // </Custom additional code>
 }
